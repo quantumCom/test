fc9d68a1aa3f07c9a4562271cb8d590911807749
txb.hk <cl.tan@dfzq.com.hk>
2019-05-09 18:28:48 +0800

    make configuration reflesh

--- client1/src/main/java/txb/test/coud/client1/controller/Client1Controller.java
+++ client1/src/main/java/txb/test/coud/client1/controller/Client1Controller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import txb.test.coud.client1.service.Client2Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@RefreshScope
 @RestController
 public class Client1Controller {
 
